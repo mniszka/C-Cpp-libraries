6a7aed065c61c429ee98c55b6e2489a363a1f42d
mniszka <aflisnik@wp.pl>
2021-04-08 19:52:37 +0200

    Create math.h

---
+++ math.h
@@ -0,0 +1,24 @@
+#include <math.h>
+
+sqrt(25) //square root
+pow(5) //power 5^2
+pow(5,3)  //5^3
+log()
+fabs() // absolute value
+  
+//rounding:
+ceil(7,45) // round up
+floor(7,45) //round down
+fmod() //remeinder of division
+  
+//comparison:
+isgreater() 
+isgreaterequal()
+isless()
+islessequal()
+islessgreater()
+
+
+
+
+
